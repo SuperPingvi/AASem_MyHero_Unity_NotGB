a5b0f131c0717dd40e98d48fc58c7345f2a5dea6
AlexS <SuperPingvi@Gmail.com>
2023-03-13 14:29:22 +0400

    Info + level transfer
    Added info in main menu.
    Fixed level transition.

--- MyHero/Assets/Scripts/LevelTeleporter.cs
+++ MyHero/Assets/Scripts/LevelTeleporter.cs
@@ -8,7 +8,9 @@ public class LevelTeleporter : MonoBehaviour
 {
     GameObject player;
     GameObject hero;
-    bool teleportEnabled;
+    bool teleportEnabled = false;
+    bool heroIn = false;
+    bool playerIn = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +21,35 @@ public class LevelTeleporter : MonoBehaviour
     {
         if(other.gameObject.tag == "Hero")
         {
-            teleportEnabled = true;
+            heroIn = true;
         }
-        if(teleportEnabled && other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            playerIn = true;
+        }
+        if(heroIn && other.gameObject.tag == "Player")
+        {
+            TeleportLevel();
+        }
+        if (playerIn && other.gameObject.tag == "Hero")
+        {
+            TeleportLevel();
         }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Hero")
+        {
+            heroIn = false;
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            playerIn = false;
+        }
+    }
+    public void TeleportLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
 }
